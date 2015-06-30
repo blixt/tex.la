4c83e1f607f0e81f0594232042bafaa3b9009927
Blixt <me@blixt.nyc>
2015-06-30 16:10:03 -0400

    MVP

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
-var http = require('http');
+var appengine = require('appengine');
+var express = require('express');
+var mathjax = require('MathJax-node/lib/mj-single');
 
-function createServer(port) {
-  var mathjax = require('MathJax-node/lib/mj-single');
-  mathjax.config({
-    MathJax: {
-      SVG: {
-        font: 'STIX-Web'
-      },
-      displayAlign: 'left',
-      displayIndent:'3em',
-      tex2jax: {
-        preview: ['[math]'],
-        processEscapes: true,
-        inlineMath: [['$','$'], ['\\(','\\)']],
-        displayMath: [['$$','$$'], ['\\[','\\]']],
-        skipTags: ['script','noscript','style','textarea','pre','code']
-      },
-      TeX: {
-        noUndefined: {disabled: true},
-      }
+mathjax.config({
+  MathJax: {
+    SVG: {
+      font: 'STIX-Web'
+    },
+    displayAlign: 'left',
+    displayIndent:'3em',
+    tex2jax: {
+      preview: ['[math]'],
+      processEscapes: true,
+      inlineMath: [['$','$'], ['\\(','\\)']],
+      displayMath: [['$$','$$'], ['\\[','\\]']],
+      skipTags: ['script','noscript','style','textarea','pre','code']
+    },
+    TeX: {
+      noUndefined: {disabled: true},
     }
-  });
-  mathjax.start();
+  }
+});
+mathjax.start();
 
-  var server = http.createServer(function (request, response) {
-    request.on('data', function (chunk) {});
-    request.on('end', function () {
-      var math = decodeURIComponent(request.url.substr(1));
-      if (!math) {
-        var example = encodeURIComponent('x = \\sum\\limits_{i=1}^n');
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write('<h1>TeX.La (LaTeX)</h1>\n');
-        response.write('<p>Usage:<br>\n');
-        response.write('<code>&lt;img height="50" src=&quot;http://tex.la/' + example + '&quot;&gt;</code></p>\n');
-        response.write('<p><img height="50" src="/' + example + '"></p>');
-        response.end();
-        return;
-      }
-      mathjax.typeset({math: math, svg: true}, function (result) {
-        if (!result.errors) {
-          response.writeHead(200, {'Content-Type': 'image/svg+xml'});
-          response.end(result.svg);
-        } else {
-          response.writeHead(400, {'Content-Type': 'text/plain'});
-          response.write('Error 400: Request Failed.\n');
-          response.write(String(result.errors) + '\n');
-          response.write(str_params + '\n');
-          response.end();
-        }
-      });
-    });
-  });
-  server.listen(port, function () {
-    console.log('Server listening on port %s' , port);
-  });
-  return server;
-}
+var app = express();
+app.use(appengine.middleware.base);
 
-var server = createServer(8000);
+app.get('/_ah/health', function (req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send('ok');
+});
+
+app.get('/_ah/start', function (req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send('ok');
+});
+
+app.get('/_ah/stop', function (req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send('ok');
+  process.exit();
+});
+
+app.get('/', function (req, res) {
+  var example = encodeURIComponent('x = \\sum\\limits_{i=1}^n i^2');
+  res.set('Content-Type', 'text/html');
+  var html = '<h1>TeX.La (LaTeX)</h1>\n' +
+             '<p>Usage:<br>\n' +
+             '<code>&lt;img height="50" src=&quot;http://tex.la/' + example + '&quot;&gt;</code></p>\n' +
+             '<p><img height="50" src="/' + example + '"></p>';
+  res.send(html);
+});
+
+app.get('/*', function (req, res) {
+  var math = decodeURIComponent(req.url.substr(1));
+  mathjax.typeset({math: math, svg: true}, function (result) {
+    if (result.errors) {
+      res.set('Content-Type', 'text/plain');
+      res.status(400).send('Error 400: Request Failed.\n' + result.errors + '\n' + math);
+      return;
+    }
+    res.set('Content-Type', 'image/svg+xml');
+    res.send(result.svg);
+  });
+});
+
+app.listen(8080, '0.0.0.0');
