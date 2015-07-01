f77f96e1945f54e1845566d89c1a1bac077586ea
Blixt <me@blixt.nyc>
2015-07-01 08:22:58 -0400

    Fix the support for query string LaTeX

--- index.js
+++ index.js
@@ -24,6 +24,36 @@ function renderToSVG(latex, callback) {
   });
 }
 
+function handleRenderRequest(latex, req, res) {
+  // Check ETag so that clients don't need to redownload SVG.
+  var etag = '"' + crypto.createHash('md5').update(pkg.version + latex).digest('base64') + '"';
+  if (req.get('If-None-Match') == etag) {
+    res.status(304).end();
+    return;
+  }
+
+  renderToSVG(latex, function (error, svg) {
+    if (error) {
+      res.set('Content-Type', 'text/plain');
+      res.status(400).send('There was an error:\n\n' + error + '\n\nInput:\n\n' + math);
+      return;
+    }
+    res.set({
+      'Cache-Control': 'public, max-age=3600',
+      'Content-Type': 'image/svg+xml',
+      'ETag': etag
+    });
+    var match = svg.match(/ width="([^"]+)ex" height="([^"]+)ex"/);
+    if (match) {
+      var newAttributes = '';
+      //newAttributes += ' width="' + (parseFloat(match[1]) * EX_TO_PX_RATIO) + 'px"';
+      newAttributes += ' height="' + Math.ceil(parseFloat(match[2]) * EX_TO_PX_RATIO) + 'px"';
+      svg = svg.substr(0, match.index) + newAttributes + svg.substr(match.index + match[0].length);
+    }
+    res.send(svg);
+  });
+}
+
 mathjax.config({
   MathJax: {
     SVG: {
@@ -56,43 +86,15 @@ app.get('/_ah/stop', function (req, res) {
 });
 
 app.get('/', function (req, res) {
+  if (req.url[1] == '?') {
+    handleRenderRequest(decodeURIComponent(req.url.substr(2)), req, res);
+    return;
+  }
   res.sendFile(__dirname + '/index.html');
 });
 
-function handleRenderRequest(latex, req, res) {
-  // Check ETag so that clients don't need to redownload SVG.
-  var etag = '"' + crypto.createHash('md5').update(pkg.version + latex).digest('base64') + '"';
-  if (req.get('If-None-Match') == etag) {
-    res.status(304).end();
-    return;
-  }
-
-  renderToSVG(latex, function (error, svg) {
-    if (error) {
-      res.set('Content-Type', 'text/plain');
-      res.status(400).send('There was an error:\n\n' + error + '\n\nInput:\n\n' + math);
-      return;
-    }
-    res.set({
-      'Cache-Control': 'public, max-age=3600',
-      'Content-Type': 'image/svg+xml',
-      'ETag': etag
-    });
-    var match = svg.match(/ width="([^"]+)ex" height="([^"]+)ex"/);
-    if (match) {
-      var newAttributes = '';
-      //newAttributes += ' width="' + (parseFloat(match[1]) * EX_TO_PX_RATIO) + 'px"';
-      newAttributes += ' height="' + Math.ceil(parseFloat(match[2]) * EX_TO_PX_RATIO) + 'px"';
-      svg = svg.substr(0, match.index) + newAttributes + svg.substr(match.index + match[0].length);
-    }
-    res.send(svg);
-  });
-}
-
 app.get('/*', function (req, res) {
-  var latex = req.url.substr(1);
-  if (latex[0] == '?') latex = latex.substr(1);
-  handleRenderRequest(decodeURIComponent(latex), req, res);
+  handleRenderRequest(decodeURIComponent(req.url.substr(1)), req, res);
 });
 
 app.listen(8080, '0.0.0.0');
