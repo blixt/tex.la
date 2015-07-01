011af42f46c22216ffd16ceae7975eddd85203c4
Blixt <me@blixt.nyc>
2015-07-01 07:59:11 -0400

    Ignore an initial ? so some escaping can be avoided
    Browsers apply all sorts of magic to the path, but not to the query
    string.

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ var mathjax = require('MathJax-node/lib/mj-single');
 var pkg = require('./package.json');
 
 var EX_TO_PX_RATIO = 7.3749;
+function renderToSVG(latex, callback) {
+  mathjax.typeset({math: latex, img: true, svg: true}, function (result) {
+    if (result.errors) {
+      callback(result.errors, null);
+      return;
+    }
+    var svg = result.svg;
+    var match = svg.match(/ width="([^"]+)ex" height="([^"]+)ex"/);
+    if (match) {
+      var newAttributes = '';
+      //newAttributes += ' width="' + (parseFloat(match[1]) * EX_TO_PX_RATIO) + 'px"';
+      newAttributes += ' height="' + Math.ceil(parseFloat(match[2]) * EX_TO_PX_RATIO) + 'px"';
+      svg = svg.substr(0, match.index) + newAttributes + svg.substr(match.index + match[0].length);
+    }
+    callback(null, svg);
+  });
+}
 
 mathjax.config({
   MathJax: {
@@ -42,19 +59,18 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/*', function (req, res) {
-  var math = decodeURIComponent(req.url.substr(1));
-
-  var etag = '"' + crypto.createHash('md5').update(pkg.version + math).digest('base64') + '"';
+function handleRenderRequest(latex, req, res) {
+  // Check ETag so that clients don't need to redownload SVG.
+  var etag = '"' + crypto.createHash('md5').update(pkg.version + latex).digest('base64') + '"';
   if (req.get('If-None-Match') == etag) {
     res.status(304).end();
     return;
   }
 
-  mathjax.typeset({math: math, img: true, svg: true}, function (result) {
-    if (result.errors) {
+  renderToSVG(latex, function (error, svg) {
+    if (error) {
       res.set('Content-Type', 'text/plain');
-      res.status(400).send('Error 400: Request Failed.\n' + result.errors + '\n' + math);
+      res.status(400).send('There was an error:\n\n' + error + '\n\nInput:\n\n' + math);
       return;
     }
     res.set({
@@ -62,7 +78,6 @@ app.get('/*', function (req, res) {
       'Content-Type': 'image/svg+xml',
       'ETag': etag
     });
-    var svg = result.svg;
     var match = svg.match(/ width="([^"]+)ex" height="([^"]+)ex"/);
     if (match) {
       var newAttributes = '';
@@ -72,6 +87,12 @@ app.get('/*', function (req, res) {
     }
     res.send(svg);
   });
+}
+
+app.get('/*', function (req, res) {
+  var latex = req.url.substr(1);
+  if (latex[0] == '?') latex = latex.substr(1);
+  handleRenderRequest(decodeURIComponent(latex), req, res);
 });
 
 app.listen(8080, '0.0.0.0');
